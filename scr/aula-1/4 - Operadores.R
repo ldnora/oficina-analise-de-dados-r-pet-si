39961dc29d6b78444ef4786cb427380a5b0012f1
Leandro Dalla Nora <workdallanora@gmail.com>
2026-03-24 22:12:50 -0300

    Feat: Adicionei novos tópicos ao material

--- scr/aula-1/4 - Operadores.R
+++ scr/aula-1/4 - Operadores.R
@@ -4,7 +4,7 @@ numero_2 <- 100
 numero_1 <- 10
 numero_2 <- 100
 
-# Aritméticos
+# aritméticos
 print(numero_1 + numero_2)
 print(numero_1 - numero_2)
 print(numero_1 * numero_2)
@@ -13,7 +13,7 @@ print(numero_2 ^ numero_1) # potência
 print(numero_2 %% numero_1) # resto da divisão 
 print(numero_2 %/% numero_1) # divisão inteira 
 
-# Relacionais
+# relacionais
 print(numero_1 == numero_2) # igualdade
 print(numero_1 != numero_2) # diferença
 print(numero_1 < numero_2) # menor
@@ -21,11 +21,12 @@ print(numero_1 > numero_2) # maior
 print(numero_1 <= numero_2) # menor ou igual 
 print(numero_1 >= numero_2) # maior ou igual 
 
-# Lógicos
+# lógicos
 print((numero_1 < numero_2) & (numero_1 > 0)) # e 
 print((numero_1 < numero_2) | (numero_1 > 100)) # ou
 print(!(numero_1 < numero_2)) # negação 
 
-# Atribuição
+# atribuição
 x <- numero_1 + numero_2 
 print(x)
+
---
+++ scr/aula-1/Desafio.R
@@ -0,0 +1,17 @@
+# Usando o dataset mtcars:
+
+# 1. Filtre apenas os carros com 4 cilindros (cyl == 4)
+
+# passo a passo:
+# importe o dataset mtcars
+# importe a biblioteca dplyr
+# use a função filter()
+
+# 2. Crie uma coluna chamada "potencia_por_peso" = hp / wt
+
+# dica:
+# use a função mutate
+
+# 3. Ordene do maior para o menor pela nova coluna
+
+# 4. Mostre apenas as colunas: mpg, hp, wt e potencia_por_peso
