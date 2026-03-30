8f707e8155fec44f1f7ab0f3dafb88dad7d5908e
Leandro Dalla Nora <workdallanora@gmail.com>
2026-03-30 20:32:31 -0300

    Feat: Adicionei novos desafios

---
+++ src/aula-2/5 - Desafio.R
@@ -0,0 +1,3 @@
+# Criar pelo menos:
+# 1 gráfico de barras simples
+# 1 gráfico com comparação usando fill
---
+++ src/aula-2/6 - Desafio.R
@@ -0,0 +1,6 @@
+# Perguntas a serem respondidas:
+# Quantas pessoas sobreviveram?
+# Quantas não sobreviveram?
+# Quem sobreviveu mais: homens ou mulheres?
+# Qual a média de idade dos passageiros?
+# Qual classe teve mais sobreviventes?
